849d92a785e150bae30452342aacf29c43dfa8ed
Pablo <pablogomesph55@gmail.com>
2025-04-12 11:41:36 -0300

    Add files via upload

---
+++ produto-card/card-produto/vite-project/src/App/App.jsx
@@ -0,0 +1,17 @@
+
+import Imagem from "../components/Imagem"
+import Container from "../components/Container"
+import "../Style/style.css"
+
+const App = () => {
+    return(
+        <>
+        <Container>
+        <Imagem />
+        </Container>
+        </>
+        
+    )
+}
+
+export default App
---
+++ produto-card/card-produto/vite-project/src/components/Imagem.jsx
@@ -0,0 +1,12 @@
+
+import ImagemPrincipal from "../assets/image-product-desktop.jpg"
+
+const Imagem = () => {
+    return(
+        <div className="imagem">
+            <img src={ImagemPrincipal} alt="" />
+        </div>
+    )
+}
+
+export default Imagem
